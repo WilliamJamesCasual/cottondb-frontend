f468a96d5137574ec1c2658b16a00213f91bebcd
WilliamJamesCasual <williamjamescasual@gmail.com>
2025-09-13 18:54:45 +0100

    Reversed changes that caused issues.

--- js/steamClient.js
+++ js/steamClient.js
@@ -14,9 +14,9 @@ fetch(apiUrl)
   .then(data => {
     console.log("Steam data:", data);
     
-    // Get a gamecount from api and show result in a target div (just to test)
-    const gameCount = data?.response?.game_count ?? "unknown";
-    outputDiv.textContent = `You own ${gameCount} games.`;
+    // Get a reference to the div with ID steam-data and swaps out the text with the number of games retrieved (just to test)
+    const outputDiv = document.getElementById("steam-data");
+    outputDiv.innerText = `You own ${data.response.game_count} games.`;
   })
 
   // Console log errors that show up as a result of the fetch call chain
