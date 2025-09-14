8a301c4d8ccd5979b354884bb42ee625a341114e
WilliamJamesCasual <williamjamescasual@gmail.com>
2025-09-14 04:00:09 +0100

    gave up on php because github pages wasn't having it

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> CottonDB. All rights reserved.</p>
-</footer>
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include('header.php');?>
-
-<body>
-  <button class="btn btn-primary">I'm a button</button>
-</body>
-</html>
-
-<?php include('footer.php');?>
